2358d7d55d42b2c8f52eeccf694eae69e47aac52
Minnowo <62451415+Minnowo@users.noreply.github.com>
2024-04-13 01:29:00 -0400

    casting highlighting

--- tree_sitter/grammar.js
+++ tree_sitter/grammar.js
@@ -156,6 +156,7 @@ module.exports = grammar({
                     '--',
                     '++',
                     '~',
+                    seq('(', $.type, ')'),
                 ),
                 $.expression)
         ),
